27cbc4c01bff7c1ae96fe349bc8356d2db065bbe
bunlong <bunlong.van@gmail.com>
2018-08-29 16:54:38 +0700

    render preact component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
 
+import Preact, { h } from "preact"
+
 export default class PreactWidget extends React.Component {
+  constructor() {
+    super();
+    this.divRef = React.createRef()
+  }
+
+  componentDidMount() {
+    this.renderPreact()
+  }
+
+  componentDidUpdate() {
+    this.renderPreact()
+  }
+
+  renderPreact() {
+    Preact.render(
+      this.props.child,
+      this.divRef.current
+    );
+  }
+
   render() {
-    return (
-      <div>
-        Hi
-      </div>
-    )
+    let h = React.createElement
+    return <div ref={this.divRef} />
   }
 }
